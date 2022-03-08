5ae8e59b21e5de3a2649b4abacdf1b24427347f1
immemx <eayresm4@gmail.com>
2022-03-08 16:42:43 -0500

    Bug with front end code seems to have been resolved, express.static seems to have fixed it

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const { notes } = require('./db/db.json')
+const path = require('path')
 
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
 app.get('/api/notes', (req, res) => {
@@ -16,7 +19,14 @@ app.get('/api/notes', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-    // Return index.html
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    const notes = createNewNotes(req.body, animals);
+    res.json(notes)
 })
 
 app.listen(80)
